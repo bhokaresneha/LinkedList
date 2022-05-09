a5aa03ace99a29ebec1686ad1c71411843bddbba
Sneha Bhokare <100981789+bhokaresneha@users.noreply.github.com>
2022-05-09 07:32:08 +0530

    Update Queue.java

--- src/Stack/Queue.java
+++ src/Stack/Queue.java
@@ -5,7 +5,7 @@ import com.bridgelabz.LinkedList;
 public class Queue {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
-        list.add(56);      //here push performs enque operation
+        list.add(56);      //here add performs enque operation
         list.add(30);
         list.add(70);
         list.display();
